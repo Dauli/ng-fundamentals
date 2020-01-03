c9dbb3451680b33754b420ad7917018312de005e
Dauli <alexisangedauli@gmail.com>
2020-01-03 15:07:00 +0200

    Using ngClass, ngSwitch...

--- src/app/events/thumbnail/thumbnail.component.ts
+++ src/app/events/thumbnail/thumbnail.component.ts
@@ -6,16 +6,40 @@ import { Component, Input} from '@angular/core'
   <div class="well hoverwell thumbnail">
   <h2>{{event?.name}}</h2>
   <div>Date: {{event?.date}}</div>
-  <div>Time: {{event.time}}</div>
+  <div [ngClass]="getStartTimeClass()"
+  [ngSwitch]="event?.time">
+    Time: {{event.time}}
+    <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+    <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+    <span *ngSwitchDefault>(Normal Start)</span>
+  </div>
   <div>Price: \${{event?.price}}</div>
   <div>
     <span>Location: {{event?.location?.address}}</span>
     <span>&nbsp;</span>
     <span>{{event?.location?.city}}, {{event?.location?.country}}</span>
   </div>
+  <div *ngIf="event?.onlineUrl">
+    Online URL: {{event?.onlineUrl}}
+  </div>
 </div>
-  `
+  `,
+  styles: [`
+    .thumbnail { min-height: 210px; }
+    .pad-left { margin-left: 10px; }
+    .well div { color: #bbb; }
+    .green { color: #003300 !important; }
+    .bold { font-weight: bold !important; }
+  `]
 })
 export class ThumbnailComponent {
   @Input() event: any
+
+  // function of custom class
+  getStartTimeClass() {
+    if(this.event && this.event.time === '8:00 am'){
+      return 'green bold'
+    }
+    return ''
+  }
 }
