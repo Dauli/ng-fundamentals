6c416476eb6fcec226bbcf18e1eebfa54143a049
Dauli <alexisangedauli@gmail.com>
2020-01-07 22:23:45 +0200

    Using Model (data type) for type safety

--- src/app/events/thumbnail/thumbnail.component.ts
+++ src/app/events/thumbnail/thumbnail.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input} from '@angular/core'
+import { IEvent } from '../shared/index'
 
 @Component({
   selector: 'thumbnail',
@@ -29,7 +30,7 @@ import { Component, Input} from '@angular/core'
   `]
 })
 export class ThumbnailComponent {
-  @Input() event: any
+  @Input() event: IEvent
 
   // function of custom class
   getStartTimeClass() {
