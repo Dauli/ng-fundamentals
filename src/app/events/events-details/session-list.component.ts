e2c2bc18a7c4af88f1b7e425bb8a3dda7d2dc957
Dauli <alexisangedauli@gmail.com>
2020-02-06 14:44:16 +0200

    Creating a filtering display

--- src/app/events/events-details/session-list.component.ts
+++ src/app/events/events-details/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges } from "@angular/core";
 import { ISession } from '../shared';
 import { faFire } from "@fortawesome/free-solid-svg-icons";
 import { faCar } from "@fortawesome/free-solid-svg-icons";
@@ -8,9 +8,32 @@ import { faCar } from "@fortawesome/free-solid-svg-icons";
   selector: 'session-list',
   templateUrl: './session-list.component.html'
 })
-export class SessionListComponent {
+export class SessionListComponent implements OnChanges {
   @Input() sessions: ISession[];
   faFire = faFire;
   faCar = faCar;
+  @Input() filterBy: string;
+  // this is a new sessions array copy for filtering, set to empty array
+  // that each time all, beginner, ... is clicked, that array is
+  // populated with the current filter to be displayed
+  visibleSessions: ISession[] = []
 
+  // We implements Onchanges to keep tracking every changes
+  // Automatically to apply filtering functionality
+  ngOnChanges() {
+    if(this.sessions){
+      this.filterSessions(this.filterBy);
+    }
+  }
+
+  // FilterSessions functionality that displays acordingly to current filter
+  filterSessions(filter) {
+    if (filter === 'all') {
+      this.visibleSessions = this.sessions.slice(0);
+    } else {
+      this.visibleSessions = this.sessions.filter( session => {
+        return session.level.toLocaleLowerCase() === filter;
+      });
+    }
+  }
 }
