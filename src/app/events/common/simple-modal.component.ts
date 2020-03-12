a3d155907daeca020bc54bb21dc0c5ccbd6aba4e
Dauli <alexisangedauli@gmail.com>
2020-03-12 19:33:15 +0200

    Using @ViewChild Decorator to close Modal dialogue

--- src/app/events/common/simple-modal.component.ts
+++ src/app/events/common/simple-modal.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, Inject } from "@angular/core";
+import { Component, Input, Inject, ViewChild, ElementRef } from "@angular/core";
+import { $ } from 'protractor';
+import { JQ_TOKEN } from "./jquery.service";
 
 @Component({
   selector: 'simple-modal',
   template: `
-    <div id="{{elementId}}" class="modal fade" tabindex="-1">
+    <div id="{{elementId}}" #modalContainer class="modal fade" tabindex="-1">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
@@ -12,7 +14,7 @@ import { Component, Input, Inject } from "@angular/core";
             </button>
             <h4 class="modal-title">{{title}}</h4>
           </div>
-          <div class="modal-body">
+          <div class="modal-body" (click)="closeModal()">
             <ng-content></ng-content>
           </div>
         </div>
@@ -29,4 +31,15 @@ import { Component, Input, Inject } from "@angular/core";
 export class SimpleModalComponent {
   @Input() title: string;
   @Input() elementId: string;
+
+  // Uses of ViewChild to close Modal dialogue
+  @ViewChild('modalContainer') containerEl: ElementRef;
+
+  // Inject jquery in constructor
+  constructor(@Inject(JQ_TOKEN) private $: any) {}
+
+  // Close modal function when click search result
+  closeModal() {
+    this.$(this.containerEl.nativeElement).modal('hide');
+  }
 }
